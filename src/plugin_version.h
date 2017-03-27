b69691cd01dc06ff59640ac42966e7e64f91bf8a
fherzog2 <fherzog2@users.noreply.github.com>
2017-03-27 11:01:58 +0200

    Bump version

--- src/plugin_version.h
+++ src/plugin_version.h
@@ -5,8 +5,8 @@
 #define INTERNAL_NAME "flif_windows_plugin.dll"
 
 #define PROJECT_MAJOR_VERSION 1
-#define PROJECT_MINOR_VERSION 1
-#define PROJECT_PATCH_VERSION 1
+#define PROJECT_MINOR_VERSION 2
+#define PROJECT_PATCH_VERSION 0
 
 #define TO_STR_HELPER(x) #x
 #define TO_STR(x) TO_STR_HELPER(x)
